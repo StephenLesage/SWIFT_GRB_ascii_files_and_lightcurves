6276e2eba0392adc0c0f2e02b7be08afd8263ca1
slesage <lesagesj@gmail.com>
2017-09-09 19:32:17 -0400

    ran the web browser script and successfully collected the .fits data for every SWIFT grb event. next i have to run the ascii file script to convert these into ascii files, then make the light curves.

--- collecting_fits_files_web_browser_navigation.py
+++ collecting_fits_files_web_browser_navigation.py
@@ -11,5 +11,5 @@ with open("swift_grbtrigger_and_burstid.txt") as f:
 		url = 'https://swift.gsfc.nasa.gov/results/batgrbcat/'+grb_name+'/data_product/'+trigger_id+'-results/lc/sw'+trigger_id+'b_4chan_64ms.lc'
 		
 		# web navigation to url and saving its contents as a .lc fits file
-		print "downloading"+grb_name+" with urllib"
+		print "downloading "+grb_name+" as sw"+trigger_id+"b_4chan_64ms.lc"
 		urllib.urlretrieve(url, 'fits_files/'+"sw"+trigger_id+"b_4chan_64ms.lc")
